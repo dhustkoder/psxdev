1e0aaecd4f6475fe9900434d80916c1535f38f21
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-01-06 15:48:42 -0200

    rev

--- src/main.c
+++ src/main.c
@@ -6,19 +6,20 @@
 #include <libetc.h>
 #include <libpad.h>
 
-#define OT_LENGTH 1 // the ordertable length
-#define PACKETMAX 18 // the maximum number of objects on the screen
+
+#define OT_LENGTH 1       // the ordertable length
+#define PACKETMAX 18      // the maximum number of objects on the screen
 #define SCREEN_WIDTH  320 // screen width
 #define	SCREEN_HEIGHT 256 // screen height (240 NTSC, 256 PAL)
 
 u_long _ramsize   = 0x00200000; // force 2 megabytes of RAM
 u_long _stacksize = 0x00004000; // force 16 kilobytes of stack
 
-static GsOT myOT[2]; // ordering table header
+static GsOT myOT[2];                       // ordering table header
 static GsOT_TAG myOT_TAG[2][1<<OT_LENGTH]; // ordering table unit
 static PACKET GPUPacketArea[2][PACKETMAX]; // GPU packet data
-static u_char paddata[2][34];   // pad 1 and 2 data
-static short CurrentBuffer = 0; // holds the current buffer number
+static u_char paddata[2][34];              // pad 1 and 2 data
+static short currrent_buffer = 0;          // holds the current buffer number
 
 
 static void graphics(void)
@@ -48,13 +49,13 @@ static void display(void)
 	FntFlush(-1);
 	
 	// get the current buffer
-	CurrentBuffer = GsGetActiveBuff();
+	currrent_buffer = GsGetActiveBuff();
 	
 	// setup the packet workbase
-	GsSetWorkBase((PACKET*)GPUPacketArea[CurrentBuffer]);
+	GsSetWorkBase((PACKET*)GPUPacketArea[currrent_buffer]);
 	
 	// clear the ordering table
-	GsClearOt(0, 0, &myOT[CurrentBuffer]);
+	GsClearOt(0, 0, &myOT[currrent_buffer]);
 	
 	// wait for all drawing to finish
 	DrawSync(0);
@@ -66,19 +67,22 @@ static void display(void)
 	GsSwapDispBuff();
 	
 	// clear the ordering table with a background color (R,G,B)
-	GsSortClear(0, 0, 0, &myOT[CurrentBuffer]);
+	GsSortClear(0, 0, 0, &myOT[currrent_buffer]);
 	
 	// draw the ordering table
-	GsDrawOt(&myOT[CurrentBuffer]);
+	GsDrawOt(&myOT[currrent_buffer]);
 }
 
 
 int main(void)
 {
-	graphics(); // setup the graphics (seen below)
+	graphics();        // setup the graphics (seen below)
 	FntLoad(960, 256); // load the font from the BIOS into the framebuffer
-	// screen X,Y | max text length X,Y | autmatic background clear 0,1 | max characters
-	SetDumpFnt(FntOpen(5, 20, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 512)); 
+
+	// screen X,Y | max text length X,Y | autmatic background 
+	// clear 0,1 | max characters
+	SetDumpFnt(FntOpen(5, 20, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 512));
+
 	PadInitDirect(&paddata[0][0], &paddata[1][0]);
 
 	for (;;) {
